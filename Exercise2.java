520b9e754b4c8408847f8e02df4bea4f2eeb2703
Claudia Hüttenrauch <claudia.huettenrauch@gmail.com>
2022-11-23 20:40:56 +0100

    added exercise 2

---
+++ Exercise2.java
@@ -0,0 +1,12 @@
+public class Exercise2 {
+    public static void main(String[] args) {
+        System.out.println("Exercise 2");
+
+        /**
+         * Write a program that prints the following to the console
+         *  a) the first character of the string, e.g. if the string is "hello", it prints "h"
+         *  b) the last letter of a string, e.g. if the string is "hello", it prints "o"
+         *  c) the first character in UPPERCASE, e.g. if the string is "hello", it prints "H"
+         */
+    }
+}

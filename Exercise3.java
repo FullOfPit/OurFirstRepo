6a2f5fe44b1301c26f2c7da764d4fc41d04563fb
Claudia Hüttenrauch <claudia.huettenrauch@gmail.com>
2022-11-23 23:01:20 +0100

    added exercise 3

--- Exercise3.java
+++ Exercise3.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Exercise3 {
+import java.util.Scanner;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        /*
+         * The following program should:
+         *    - read a word from the terminal, that the user types in
+         *    - print "Valid", if the given word starts with an "a" or "A" and has more than 5 letters
+         *    - print "Not valid" otherwise
+         *
+         * The program currently does not work as expected, e.g. although "Apfelbaum" is a valid string, it prints "Not valid".
+         * Fix it.
+         */
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter a word:");
+        String word = scanner.next();
+
+        if (!isValid(word)) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Not valid");
+        }
+    }
+
+    public static boolean isValid(String str) {
+        return str.toLowerCase().startsWith("a") && str.length() > 5;
+    }
 }

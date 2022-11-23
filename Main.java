e600890e17e9f29f02362434a44ccd9fa3b35e6b
Christian Weck <weck.chr@gmail.com>
2022-11-23 19:39:55 +0100

    First Commit: Dem Questions

---
+++ Main.java
@@ -0,0 +1,60 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Glück Auf aus dem Erzgebirge!");
+
+        /*
+        declare and initialise a string firstName
+        declare and initialise a string lastName
+
+        declare a method that will take two strings, a first name and a last name
+                and gives prints the full name to the console
+
+        declare a method that will take two strings, a first name and a last name
+                and returns the full name
+         */
+
+        /*
+        declare a string firstName
+        declare a string lastName
+
+        declare a method that takes a string parameter firstName, sets the variable firstName to that parameter
+            and returns nothing
+        declare a method that takes a string parameter lastName, sets the variable lastName to that parameter
+            and returns nothing
+
+        declare a method that takes two strings as parameters, firstName and lastName, prints the full name to
+            the console and returns nothing
+
+         */
+
+        /*
+        declare a string firstName
+        declare a string lastName
+
+        declare a method that takes a string parameter firstName, sets the variable firstName to that parameter
+            and returns nothing
+        declare a method that takes a string parameter lastName, sets the variable lastName to that parameter
+            and returns nothing
+
+        declare a method that takes two strings as parameters, firstName and lastName, prints the full name to
+            the console and returns nothing
+
+         */
+
+        /*
+        initialise a string fullName
+        initialise an int age
+        declare a method that will set the fullName
+        declare a method that will set the age
+
+        declare a method that takes two parameters, a string and an age and prints one string
+        of the following formats to the console:
+        1)  My name is <fullName> and I am <age> years old
+        2)  My name is <fullName> and I am a lady so my age is none of your business (it's <age> though)
+         */
+
+        /*
+        declare and initialise a
+         */
+    }
+}

5ee94de4b9cdf643efdb69c480b77c3a1f9e1ed4
NameGitHub <nachts@freenet.de>
2022-11-23 21:42:48 +0100

    Solution added

--- Main.java
+++ Main.java
@@ -7,13 +7,27 @@ public class Main {
         declare and initialise a string lastName
 
         declare a method that will take two strings, a first name and a last name
-                and gives prints the full name to the console
+                and prints the full name to the console
 
         declare a method that will take two strings, a first name and a last name
                 and returns the full name
          */
+    String firstName = "Doreen";
+    String lastName = "Mier";
 
+        System.out.println(fullName1(firstName, lastName));
 
+        fullName2(firstName, lastName);
 
+    }
+
+    public static String fullName1(String eins, String zwei){
+
+        return (eins + " " + zwei);
+    }
+
+    public static void fullName2(String eins, String zwei){
+
+        System.out.println(fullName1(eins, zwei));
     }
 }

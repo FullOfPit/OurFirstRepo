17526fc1c5d803a08ec0a00499f160933b978056
Christian Weck <weck.chr@gmail.com>
2022-11-23 20:02:43 +0100

    Second Commit: Less Question

--- Main.java
+++ Main.java
@@ -13,48 +13,7 @@ public class Main {
                 and returns the full name
          */
 
-        /*
-        declare a string firstName
-        declare a string lastName
 
-        declare a method that takes a string parameter firstName, sets the variable firstName to that parameter
-            and returns nothing
-        declare a method that takes a string parameter lastName, sets the variable lastName to that parameter
-            and returns nothing
 
-        declare a method that takes two strings as parameters, firstName and lastName, prints the full name to
-            the console and returns nothing
-
-         */
-
-        /*
-        declare a string firstName
-        declare a string lastName
-
-        declare a method that takes a string parameter firstName, sets the variable firstName to that parameter
-            and returns nothing
-        declare a method that takes a string parameter lastName, sets the variable lastName to that parameter
-            and returns nothing
-
-        declare a method that takes two strings as parameters, firstName and lastName, prints the full name to
-            the console and returns nothing
-
-         */
-
-        /*
-        initialise a string fullName
-        initialise an int age
-        declare a method that will set the fullName
-        declare a method that will set the age
-
-        declare a method that takes two parameters, a string and an age and prints one string
-        of the following formats to the console:
-        1)  My name is <fullName> and I am <age> years old
-        2)  My name is <fullName> and I am a lady so my age is none of your business (it's <age> though)
-         */
-
-        /*
-        declare and initialise a
-         */
     }
 }

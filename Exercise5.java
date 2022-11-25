5b3be03bdd482950a4c5ccb00ac84a73c0759723
Christian Weck <weck.chr@gmail.com>
2022-11-25 14:20:42 +0100

    Exercise 5 added

---
+++ Exercise5.java
@@ -0,0 +1,25 @@
+
+
+public class Exercise5 {
+
+    public static void main(String[] args) {
+        /*
+
+                                -- Brace yourselves, this is a tough nut to crack --
+
+
+        Write a program that:
+            - takes a word from the user through the console
+            - decides which data type the input resembles
+            - will print the respective data type and the corresponding input in the following format:
+                 "Your input <input> is suggested to be a <data type>"
+            - asks the user to decide whether the suggested data type is correct; confirm is so, deny if not
+            - if the suggested data type is correct:
+                calls a separate method that will return the input in its respective suggested data type
+            - if the suggested data type is incorrect:
+                tests whether the input can be converted into the data type the user suggests or not
+                - if so: calls a separate method that will return the input in the respective data type the user suggested
+                - if not: tells the user that the data types cannot be converted (easily)
+         */
+    }
+}

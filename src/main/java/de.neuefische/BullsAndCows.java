955a063bb8690c78d368a28435e266fc5cb50f58
Claudia Hüttenrauch <claudia.huettenrauch@gmail.com>
2022-11-26 17:26:04 +0100

    added bulls and cows exercise

---
+++ src/main/java/de.neuefische/BullsAndCows.java
@@ -0,0 +1,38 @@
+package de.neuefische;
+
+/*
+ * ------------------------------------------------------------------------------
+ * BULLS AND COWS (aka Mastermind)
+ * ------------------------------------------------------------------------------
+ * The user must guess a 4-digit secret code.
+ * If the matching digits are in their right positions, they are "bulls", if in different positions, they are "cows".
+ *
+ * Example:
+ *      secret code: 4271
+ *      users guess: 1234
+ *      Result: 1 bull and 2 cows. (The bull is "2", the cows are "4" and "1")
+ * ------------------------------------------------------------------------------
+ *
+ * Tasks:
+ *      [ ] set a secret code
+ *      [ ] let the user enter a guess
+ *      [ ] compare the guess with the secret code (rules see above)
+ *      [ ] output the result in the format "%d bull(s) and %d cow(s)."
+ *      [ ] if the user guessed the secret code correct:
+ *          - output "You found the secret code. Congratulations!"
+ *          - the game then stops
+ *      [ ] if the user does not guess the secret code:
+ *          - ask for the next guess
+ *
+ * ------------------------------------------------------------------------------
+ *
+ * Bonus:
+ *      [ ] randomly generate the secret code (consists of 4 digits from 0-9, the digits must be all different)
+ *      [ ] implement a maximum number of turns the user has to guess the secret code
+ *      [ ] validate the user input (the rules are the same as for the secret code generation)
+ */
+public class BullsAndCows {
+    public static void main(String[] args) {
+
+    }
+}
